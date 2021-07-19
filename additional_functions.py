6ae0b1f69293680922422ecd9e98a3bee54ffde9
hhalvorsen <83094292+hhalvorsen@users.noreply.github.com>
2021-07-19 14:13:32 +0200

    første forsøk, virker ikke

--- additional_functions.py
+++ additional_functions.py
@@ -1,9 +1,7 @@
 import re
-import codecs
 
 
 def separate_name_and_height(info):
-    print(info)
     # Separate mountain name and height
     mountain_height = re.findall('[0-9]+', info)[0]
     mountain_name_match = re.search('[0-9]+', info)
@@ -11,13 +9,15 @@ def separate_name_and_height(info):
     return [mountain_name, mountain_height]
 
 
+"""
+# Testing functions
 # Read mountain peaks from file
+import codecs # Yes, wrong place to put import
 with codecs.open('test.txt', encoding='utf8') as f:
     lines = f.readlines()
 
-
-# Testing functions
 test_list = separate_name_and_height(lines[0])
 print(test_list[0])
 print(test_list[1])
+"""
 
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.support.ui import Select
 import codecs
 import additional_functions as af
 # Script made to automatically add the mountains I've climbed from my list into peakbook
@@ -14,16 +15,20 @@ notAdded = ["test"]
 # Open peakbook.org and log in
 # Using Chrome to access web
 driver = webdriver.Chrome()
+
 # Open the website
 driver.get('https://peakbook.org')
+
 # Open log in dialog
 open_login_button = driver.find_element_by_class_name('login')
 open_login_button.click()
+
 # Enter username and password
 username_box = driver.find_element_by_name('logindata[username]')
 username_box.send_keys('hhalvorsen')
 password_box = driver.find_element_by_name('logindata[password]')
 password_box.send_keys('Jul38rus')
+
 # Log in
 login_button = driver.find_element_by_name('login')
 login_button.click()
@@ -32,27 +37,44 @@ login_button.click()
 for mountain_string in lines:
     # Separate mountain name and height
     mountain_info = af.separate_name_and_height(mountain_string)
-    print(mountain_info)
-    """
+
     # Open "Register new trip"-dialog
     register_trip_button = driver.find_element_by_link_text('Registrer tur')
     register_trip_button.click()
+
     # Add title
     title_box = driver.find_element_by_name('formdata[title]')
-    title_box.send_keys('hhalvorsen')
-    """
+    title_box.send_keys(mountain_info[0])
 
     # Check for unknown time of ascent
+    time_unknown_box = driver.find_element_by_name('formdata[dateUnknown]')
+    time_unknown_box.click()
+
     # Enter mountain into search field
+    mountain_box = driver.find_element_by_id('toursPBEs_input')
+    mountain_box.send_keys(mountain_info[0])
+
     # Choose correct mountain
+    # Get options from drop down menu
+    drop_down_selector = Select(driver.find_element_by_class_name('suggestions'))
+    drop_down_options = drop_down_selector.options
+
+    mountain_match = True
+    # If no mountain has the same name
+    if len(drop_down_options) == 0:
+        notAdded.append(mountain_info[0] + ' ' + mountain_info[1])
+        mountain_match = False
+    else:
+        for option in drop_down_options:
+            option_height = option.find_element_by_class_name('suggestor_text')
+            print(option_height.text)
         # If unable to find mountain, add to notAdded list and continue to next mountain
     # Press home button
+    break
+
 
 # Save notAdded in text file
 f2 = open("notAdded.txt", "w")
 f2.writelines(notAdded)
 f2.close()
-
-
-print("Ønske")
 
